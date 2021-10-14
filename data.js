de7c3050f6a5d503740db3eb074a75feef1c8955
NonoL3Robot <75622909+NonoL3Robot@users.noreply.github.com>
2021-10-14 09:33:06 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,102 +1,102 @@
 let data = [
 	{
 		id: 1,
-		question:,
-		answers: [,]
+		question:"You are a hacker and you conceived a virus: do you sell it?",
+		answers: ["Yes","No"]
 	},
 	{
 		id: 2,
-		question:,
-		answers: [,]
+		question:"You have to protect the NASA from cyber attacks, what do you do?",
+		answers: ["I implement a password manager","I raise employee awareness"]
 	},
 	{
 		id: 3,
-		question: "",
-		answers: ["",""]
+		question: "Do you send a phishing email to E-Corp (a NASA subsidiary) with your virus?",
+		answers: ["Yes","No"]
 	},
 	{
 		id: 4,
-		question: "",
-		answers: ["",""]
+		question: "Today, half of the company's computers have crashed: all the processors have imploded and caused a fire in the building, what do you do?",
+		answers: ["I put water to put out the fire","I cut the company's power supply in an emergency"]
 	},
 	{
 		id: 5,
-		question: "",
-		answers: ["",""]
+		question: "Do I get information about NASA employees?",
+		answers: ["Yes","No"]
 	},
 	{
 		id: 6,
-		question:"",
-		answers: ["",""]
+		question:"Do you update the company's VPS virus database?",
+		answers: ["Yes","No"]
 	},
 	{
 		id: 7,
-		question:"",
-		answers: ["",""]
+		question:"You develop a Trojan horse, do you want to publish it on the darkweb?",
+		answers: ["Yes","No"]
 	},
 	{
 		id: 8,
-		question:"",
-		answers: ["",""]
+		question:"Do you spend all day surfing hacking forums?",
+		answers: ["Yes","No"]
 	},
 	{
 		id: 9,
-		question:"",
-		answers: ["",""]
+		question:"Do you want to launch a DDoS attack on the delivery website of the local pizzeria?",
+		answers: ["Yes","No"]
 	},
 	{
 		id: 10,
-		question:"",
-		answers: ["",""]
+		question:"Although you have tried to protect the company, you are a victim of ransomware, what do you do?",
+		answers: ["I disconnect all the power supplies and the company network","I wait for the director to pay the ransom"]
 	},
 	{
 		id: 11,
-		question:"",
-		answers: ["",""]
+		question:"To protect your money do you convert it into Bitcoin?",
+		answers: ["Yes","No"]
 	},
 	{
 		id: 12,
-		question:"",
-		answers: ["",""]
+		question:"You have discovered a virus with interesting technology.",
+		answers: ["You try to trace the hacker from it","You show it to the police and inform the director"]
 	},
 	{
 		id: 13,
-		question:"",
-		answers: ["",""]
+		question:"You become more and more known in the hacking world and you decide to set up a team, do you attack the NSA?",
+		answers: ["Yes","No"]
 	},
 	{
 		id: 14,
-		question:"",
-		answers: ["",""]
+		question:"While surfing on darkweb forums, you heard about the creation of a suspicious team of hackers.",
+		answers: ["You try to infiltrate them","You hire someone to track them down"]
 	},
 	{
 		id: 15,
-		question:"",
-		answers: ["",""]
+		question:"A member of your team reported you to the authorities, what do you do?",
+		answers: ["I move all my money to another bank and change my identity","I move to another country"]
 	},
 	{
 		id: 16,
-		question:"",
-		answers: ["",""]
+		question:"You just got a promotion for your investment in the company. Do you invite your friends to celebrate this good news?",
+		answers: ["Yes","No, I'm already invited to another party"]
 	},
 	{
 		id: 17,
-		question:"",
-		answers: ["",""]
+		question:"Friends invite me to a party, do you go?",
+		answers: ["Yes, no worries, my group will carry out an attack during the night","No, I could be attacked when I'm away"]
 	},
 	{
 		id: 18,
-		question:"",
-		answers: ["",""]
+		question:"During your party you get a call from your boss who informs you that someone has entered the HoneyPot you set up.",
+		answers: ["I continue the party because I am confident in my technology","I leave the party and drive back to work (drink driving again...)"]
 	},
 	{
 		id: 19,
-		question:"",
-		answers: ["",""]
+		question:"You program a website that sells the NASA information, do you put up the information of your friend who works there?",
+		answers: ["Yes","No"]
 	},
 	{
 		id: 20,
-		question:"",
-		answers: ["",""]
+		question:"You hack the website and have access to the information of the hackers, do you want to reveal their identities to the public?",
+		answers: ["Yes","No"]
 	}
 ];
