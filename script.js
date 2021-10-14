cbe64b721ab9ba723bd1f18115b437c00319bbae
LucasV-IUT <81527471+LucasV-IUT@users.noreply.github.com>
2021-10-14 09:20:46 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,102 @@
+let data = [
+	{
+		id: 1,
+		question:,
+		answers: [,]
+	},
+	{
+		id: 2,
+		question:,
+		answers: [,]
+	},
+	{
+		id: 3,
+		question: "",
+		answers: ["",""]
+	},
+	{
+		id: 4,
+		question: "",
+		answers: ["",""]
+	},
+	{
+		id: 5,
+		question: "",
+		answers: ["",""]
+	},
+	{
+		id: 6,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 7,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 8,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 9,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 10,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 11,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 12,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 13,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 14,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 15,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 16,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 17,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 18,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 19,
+		question:"",
+		answers: ["",""]
+	},
+	{
+		id: 20,
+		question:"",
+		answers: ["",""]
+	}
+];
