672806d493b8d60fbc0d34b34a0250ec69540bcd
LucasVbr <vabrelucas@gmail.com>
2021-10-14 15:57:27 +0200

    final

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ let expCyber = 0;
 
 function main() {
 	
-	// test
-	expCyber = 10;
-	
 	nextTurn(turn);
 	answersCanvasElement.style.visibility = "visible";
 	turnCanvasElement.style.visibility = "visible";
@@ -30,12 +27,24 @@ function nextTurn(id) {
 			/* Hacker Turn */
 			turnTitle.classList.remove("turn-cyber");
 			turnTitle.classList.add("turn-hacker");
+
+			answerOneInput.style.boxShadow = "var(--red) 0px 0px 25px";
+			answerTwoInput.style.boxShadow = "var(--red) 0px 0px 25px";
+
+			expHacker += Math.floor(1+ Math.random() * 4);
 		} else {
 			/* Cyber Turn */
 			turnTitle.classList.remove("turn-hacker");
 			turnTitle.classList.add("turn-cyber");
+
+			answerOneInput.style.boxShadow = "var(--blue) 0px 0px 25px";
+			answerTwoInput.style.boxShadow = "var(--blue) 0px 0px 25px";
+
+			expCyber += Math.floor(1 + Math.random() * 4);
 		}
 		
+		console.log(`expCyber:${expCyber}\nexpHacker:${expHacker}`);
+
 		titleElement.innerText = data[id].question;
 		answerOneInput.innerText = data[id].answers[0];
 		answerTwoInput.innerText = data[id].answers[1];
